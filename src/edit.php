25d72b61e3cfccd8805c6c7bdbef81c512fb9f2f
이신우 <tlsdn0625@g.yju.ac.kr>
2025-12-25 12:15:23 +0900

    수정기능 구현

--- src/edit.php
+++ src/edit.php
@@ -1,1 +1,29 @@
-<?php // 게시판 수정 구현
+<?php
+include 'db.php';
+
+$id = $_GET['id'];
+$sql = "SELECT * FROM posts WHERE id = $id";
+$result = mysqli_query($conn, $sql);
+$post = mysqli_fetch_assoc($result);
+
+if ($_POST) {
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $update_sql = "UPDATE posts SET title = '$title', content = '$content' WHERE id = $id";
+    
+    mysqli_query($conn, $update_sql);
+    echo "<script> location.href='post.php?id=$id'; </script>";
+    exit;
+}
+?>
+<h2>글 수정</h2>
+<form method="post">
+    제목<br>
+    <input type="text" name="title" value="<?= $post['title'] ?>" required><br><br>
+
+    내용<br>
+    <textarea name="content" rows="10" required><?= $post['content'] ?></textarea><br><br>
+
+    <button type="submit">수정</button>
+</form>
+<a href="post.php?id=<?= $id ?>">취소</a>
